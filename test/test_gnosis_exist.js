b65c6eb28587e2e640c4f71461739624029b0a2b
Alan Lu <alanlu1023@gmail.com>
2017-06-13 14:04:53 -0500

    Added API spec to tests

--- test/test_gnosis_exist.js
+++
@@ -1,6 +0,0 @@
-var assert = require('assert');
-  describe('#sample test', function() {
-    it('tests out of bounds array', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
