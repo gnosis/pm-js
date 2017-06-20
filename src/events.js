e1897cacbab03b569d1b46bf8293fbf0438557f4
Alan Lu <alanlu1023@gmail.com>
2017-06-20 09:33:55 -0500

    Wrote API documentation for methods

--- src/events.js
+++ src/events.js
@@ -1,6 +1,14 @@
 import _ from 'lodash'
 import { getTruffleArgsFromOptions, sendTransactionAndGetResult } from './utils'
 
+/**
+ * Creates a categorical event.
+ * @param {Contract|string} opts.collateralToken - The collateral token contract or its address
+ * @param {Contract|string} opts.oracle - The oracle responsible for resolving this event
+ * @param {Number|string|BigNumber} opts.outcomeCount - The number of outcomes of this event
+ * @returns {Contract} The created categorical event
+ * @alias Gnosis.createCategoricalEvent
+ */
 export async function createCategoricalEvent(opts) {
     let args = getTruffleArgsFromOptions([
         'collateralToken',
@@ -18,6 +26,15 @@ export async function createCategoricalEvent(opts) {
     })
 }
 
+/**
+ * Creates a scalar event.
+ * @param {Contract|string} opts.collateralToken - The collateral token contract or its address
+ * @param {Contract|string} opts.oracle - The oracle responsible for resolving this event
+ * @param {Number|string|BigNumber} opts.lowerBound - The lower bound for the event outcome
+ * @param {Number|string|BigNumber} opts.upperBound - The upper bound for the event outcome
+ * @returns {Contract} The created scalar event
+ * @alias Gnosis.createScalarEvent
+ */
 export async function createScalarEvent(opts) {
     let args = getTruffleArgsFromOptions([
         'collateralToken',
@@ -36,10 +53,22 @@ export async function createScalarEvent(opts) {
     })
 }
 
+/**
+ * Publishes an event description onto IPFS.
+ * @param {Object} description - A POD object describing the event
+ * @returns {string} The IPFS hash locating the published event
+ * @alias Gnosis.publishEventDescription
+ */
 export async function publishEventDescription(description) {
     return await this.ipfs.addJSONAsync(description)
 }
 
+/**
+ * Loads an event description from IPFS.
+ * @param {string} ipfsHash - The IPFS hash locating the published event
+ * @returns {Object} A POD object describing the event
+ * @alias Gnosis.loadEventDescription
+ */
 export async function loadEventDescription(ipfsHash) {
     return await this.ipfs.catJSONAsync(ipfsHash)
 }
--- src/markets.js
+++ src/markets.js
@@ -1,6 +1,17 @@
 import _ from 'lodash'
 import { getTruffleArgsFromOptions, sendTransactionAndGetResult } from './utils'
 
+/**
+ * Creates a market.
+ * @param {Contract|string} opts.forwardedOracle - The forwarded oracle contract or its address
+ * @param {Contract|string} opts.collateralToken - The collateral token contract or its address
+ * @param {Number|string|BigNumber} opts.spreadMultiplier - The spread multiplier
+ * @param {Number|string|BigNumber} opts.challengePeriod - The challenge period in seconds
+ * @param {Number|string|BigNumber} opts.challengeAmount - The amount of collateral tokens put at stake in the challenge
+ * @param {Number|string|BigNumber} opts.frontRunnerPeriod - The front runner period in seconds
+ * @returns {Contract} The created ultimate oracle contract instance
+ * @alias Gnosis.createUltimateOracle
+ */
 export async function createMarket(opts) {
     let args = getTruffleArgsFromOptions([
         'event',
