6804e57990f7175176c6fa9d79274047016789cf
Alan Lu <alanlu1023@gmail.com>
2017-06-20 13:37:11 -0500

    Fuss over packaging

--- README.md
+++ README.md
@@ -8,10 +8,10 @@ Needs a web3 to work. For testing, try TestRPC.
 
 ## Building and the frontend
 
-Run `npm run dev` to compile and migrate the contracts, build the app, and serve it on http://localhost:8080.
+Run `npm run dev` to compile and migrate the contracts and continously rebuild the library.
 
 ## Testing
 
-Before any test runs, be sure contracts are compiled and migrated. You can do that by running `npm run cnm`.
+Before any test runs, be sure contracts are compiled and migrated. You can do that by running `npm run migrate`.
 
 Run `npm test` to run test suite.
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,25 @@
-const fs = require('fs')
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: {
-    // previously needed for default truffle app
-    // app: './app/javascripts/app.js',
-    gnosis: './src/index.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].[chunkhash].js',
-    library: '[name]'
-  },
-  plugins: [new HtmlWebpackPlugin({
-    // previously needed for default truffle app
-    // template: 'app/index.ejs'
-  })],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-      },
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'gnosis.js',
+        library: 'Gnosis'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
       { test: /\.json$/, use: 'json-loader' },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: JSON.parse(fs.readFileSync(path.resolve(__dirname, '.babelrc')))
-      }
-    ]
-  },
-  devtool: 'source-map'
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    devtool: 'source-map'
 }
