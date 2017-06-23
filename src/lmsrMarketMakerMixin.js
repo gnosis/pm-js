3ec3102a18c746cd53ee5087af3c394ff92e64a1
Alan Lu <alanlu1023@gmail.com>
2017-06-23 08:30:44 -0500

    Moved LMSR calcCost off RPC, but still gets market state from RPC calls

--- src/lmsrMarketMakerMixin.js
+++ src/lmsrMarketMakerMixin.js
@@ -1,6 +1,22 @@
 import _ from 'lodash'
 import { getTruffleArgsFromOptions, Decimal } from './utils'
 
+// TODO: Figure out a better way to do this:
+async function getOutcomeCountFromMarket(market, Event) {
+    return Number((
+        await (
+            await Event.at(
+                await market.eventContract()
+            )
+        ).getOutcomeCount()).toString())
+}
+
+async function getNetOutcomeTokensSoldFromMarket(market, outcomeCount) {
+    return (await Promise.all(_.range(outcomeCount).map(
+        (i) => market.netOutcomeTokensSold(i)
+    ))).map((v) => new Decimal(v.toString()))
+}
+
 /**
  * Calculates the cost of buying specified number of outcome tokens from LMSR market.
  * @param {Contract|string} opts.market - Market to calculate LMSR costs on
@@ -9,12 +25,28 @@ import { getTruffleArgsFromOptions, Decimal } from './utils'
  * @returns {BigNumber} The cost of the outcome tokens in event collateral tokens
  */
 export async function calcCost (opts) {
-    let args = getTruffleArgsFromOptions([
-        'market',
-        'outcomeTokenIndex',
-        'outcomeTokenCount'
-    ], opts)
-    return await this._calcCost.call(...args)
+    let { market, outcomeTokenIndex, outcomeTokenCount } = opts
+    outcomeTokenCount = new Decimal(outcomeTokenCount.toString())
+
+    let outcomeCount = await getOutcomeCountFromMarket(market, this.gnosis.contracts.Event)
+    let netOutcomeTokensSold = await getNetOutcomeTokensSoldFromMarket(market, outcomeCount)
+    let b = new Decimal((await market.funding()).toString()).dividedBy(new Decimal(outcomeCount).ln())
+
+    return b.times(
+        netOutcomeTokensSold.reduce((acc, numShares, i) =>
+            acc.plus(
+                new Decimal(numShares.toString())
+                .plus(i === outcomeTokenIndex ? outcomeTokenCount : 0)
+                .dividedBy(b)
+                .exp()),
+            new Decimal(0)).ln()
+        .minus(netOutcomeTokensSold.reduce((acc, numShares) =>
+            acc.plus(
+                new Decimal(numShares.toString())
+                .dividedBy(b)
+                .exp()),
+            new Decimal(0)).ln()
+        )).ceil()
 }
 
 /**
@@ -27,19 +59,12 @@ export async function calcCost (opts) {
 export async function calcOutcomeTokenCount (opts) {
     // decimal.js making this reaaally messy :/
     let { market, outcomeTokenIndex, cost } = opts
-    cost = new Decimal(cost.toString())
+    cost = new Decimal(cost.toString()).minus(0.5)
 
-    // TODO: Figure out a better way to do this:
-    let outcomeCount = Number((
-        await (
-            await this.gnosis.contracts.Event.at(
-                await market.eventContract()
-            )
-        ).getOutcomeCount()).toString())
-
-    let netOutcomeTokensSold = (await Promise.all(_.range(outcomeCount).map((i) =>
-    market.netOutcomeTokensSold(i)))).map((v) => new Decimal(v.toString()))
+    let outcomeCount = await getOutcomeCountFromMarket(market, this.gnosis.contracts.Event)
+    let netOutcomeTokensSold = await getNetOutcomeTokensSoldFromMarket(market, outcomeCount)
     let b = new Decimal((await market.funding()).toString()).dividedBy(new Decimal(outcomeCount).ln())
+
     return b.times(
         netOutcomeTokensSold.reduce((acc, numShares) =>
             acc.plus(
