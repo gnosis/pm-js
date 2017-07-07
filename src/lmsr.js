86a2de436c6b767563e8f81bb5e70352c73eb6b0
Alan Lu <alanlu1023@gmail.com>
2017-07-07 09:27:19 -0500

    Fix some docs

--- src/lmsr.js
+++ src/lmsr.js
@@ -3,8 +3,8 @@ import { getTruffleArgsFromOptions, Decimal } from './utils'
 
 /**
  * Estimates the cost of buying specified number of outcome tokens from LMSR market.
- * @param {(number[]|string[]|BigNumber[])} netOutcomeTokensSold - Amounts of net outcome tokens that have been sold. Negative amount means more have been bought than sold.
- * @param {(number|string|BigNumber)} funding - The amount of funding market has
+ * @param {(number[]|string[]|BigNumber[])} opts.netOutcomeTokensSold - Amounts of net outcome tokens that have been sold. Negative amount means more have been bought than sold.
+ * @param {(number|string|BigNumber)} opts.funding - The amount of funding market has
  * @param {(number|string|BigNumber)} opts.outcomeTokenIndex - The index of the outcome
  * @param {(number|string|BigNumber)} opts.outcomeTokenCount - The number of outcome tokens to buy
  * @returns {Decimal} The cost of the outcome tokens in event collateral tokens
@@ -35,8 +35,8 @@ export function calcLMSRCost (opts) {
 
 /**
  * Estimates the number of outcome tokens which can be purchased by specified amount of collateral.
- * @param {Number[]|string[]|BigNumber[]} netOutcomeTokensSold - Amounts of net outcome tokens that have been sold. Negative amount means more have been bought than sold.
- * @param {(number|string|BigNumber)} funding - The amount of funding market has
+ * @param {(Number[]|string[]|BigNumber[])} opts.netOutcomeTokensSold - Amounts of net outcome tokens that have been sold. Negative amount means more have been bought than sold.
+ * @param {(number|string|BigNumber)} opts.funding - The amount of funding market has
  * @param {(number|string|BigNumber)} opts.outcomeTokenIndex - The index of the outcome
  * @param {(number|string|BigNumber)} opts.outcomeTokenCount - The amount of collateral for buying tokens
  * @returns {Decimal} The number of outcome tokens that can be bought
