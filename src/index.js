029db340f75d944138737aed6b0c0c75b3bf5106
Alan Lu <alanlu1023@gmail.com>
2017-12-29 10:08:30 -0600

    Relax mainnet check to allow Numbers being returned by web3

--- src/index.js
+++ src/index.js
@@ -218,7 +218,7 @@ class Gnosis {
              * @member {Contract} Gnosis#etherToken
              */
             (async () => {
-                if(await utils.promisify(this.web3.version.getNetwork)() === '1') {
+                if(await utils.promisify(this.web3.version.getNetwork)() == 1) {
                     this.etherToken = this.contracts.EtherToken.at('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
                 } else {
                     await this.trySettingContractInstance('etherToken', this.contracts.EtherToken)
